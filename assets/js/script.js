7cdf834ac72f571b6d46d1d5649545e6e4ecbaac
Arian Mahdavi <amahdavi9978@gmail.com>
2022-02-03 10:44:29 +0330

    config tab.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
   function menuscroll() {
     let navmenu = $(".header__nav");
@@ -38,3 +36,60 @@ $(document).ready(function () {
     }
   );
 });
+
+let tab1 = document.getElementById("tab1");
+let tab2 = document.getElementById("tab2");
+let tab3 = document.getElementById("tab3");
+
+let tabContent1 = document.getElementById("tab-content-1");
+let tabContent2 = document.getElementById("tab-content-2");
+let tabContent3 = document.getElementById("tab-content-3");
+
+tab1.addEventListener("click", function () {
+  tabContent2.style.display = "none";
+  tabContent3.style.display = "none";
+  tabContent1.style.display = "block";
+  tab1.classList.add("active-tab");
+  tab2.classList.remove("active-tab");
+  tab3.classList.remove("active-tab");
+});
+
+tab2.addEventListener("click", function () {
+  tabContent2.style.display = "block";
+  tabContent1.style.display = "none";
+  tabContent3.style.display = "none";
+  tab2.classList.add("active-tab");
+  tab1.classList.remove("active-tab");
+  tab3.classList.remove("active-tab");
+});
+
+tab3.addEventListener("click", function () {
+  tabContent3.style.display = "block";
+  tabContent1.style.display = "none";
+  tabContent2.style.display = "none";
+  tab3.classList.add("active-tab");
+  tab1.classList.remove("active-tab");
+  tab2.classList.remove("active-tab");
+});
+
+fetch("http://griffinmart.com/api/resumes")
+  .then(function (response) {
+    // The API call was successful!
+    console.log(response.json());
+
+    return response.json();
+  })
+  .then(function (data) {
+    // This is the JSON from our response
+    console.log(data);
+  })
+  .catch(function (err) {
+    // There was an error
+    console.warn("Something went wrong.", err);
+  });
+
+let name = document.getElementById("fname");
+let formData = new FormData();
+
+formData.append("name", name);
+fetch("/resumes", { method: "POST", body: formData });
